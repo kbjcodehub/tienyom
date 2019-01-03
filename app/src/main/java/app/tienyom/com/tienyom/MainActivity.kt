5bf74436d2e7377ad1eceee82c18fec168847d8f
KYEI BARFOUR <kyeibarfour@KYEIs-MacBook-Pro.local>
2019-01-03 18:21:16 +0000

    hello world

---
+++ app/src/main/java/app/tienyom/com/tienyom/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.tienyom.com.tienyom
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
